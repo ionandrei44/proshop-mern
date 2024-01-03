d95ec6ed89b86f3f25035f3be0b76889ac79d7b0
Andrei <44devtests@gmail.com>
2024-01-03 22:18:56 +0200

    delete carousel loader

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,7 +1,6 @@
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
-import Loader from "../components/Loader";
 import Message from "../components/Message";
 import Paginate from "../components/Paginate";
 import ProductCarousel from "../components/ProductCarousel";
@@ -11,7 +10,7 @@ import { useParams, Link } from "react-router-dom";
 const HomeScreen = () => {
   const { pageNumber, keyword } = useParams();
 
-  const { data, isLoading, error } = useGetProductsQuery({
+  const { data, error } = useGetProductsQuery({
     keyword,
     pageNumber,
   });
@@ -26,9 +25,7 @@ const HomeScreen = () => {
         </Link>
       )}
 
-      {isLoading ? (
-        <Loader />
-      ) : error ? (
+      {error ? (
         <Message variant="danger">
           {error?.data?.message || error.error}
         </Message>
@@ -38,15 +35,15 @@ const HomeScreen = () => {
           <h1>Latest Products</h1>
 
           <Row>
-            {data.products.map((product) => (
+            {data?.products?.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
             ))}
           </Row>
           <Paginate
-            page={data.page}
-            pages={data.pages}
+            page={data?.page}
+            pages={data?.pages}
             keyword={keyword ? keyword : ""}
           />
         </>
